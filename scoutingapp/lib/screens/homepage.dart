47ac205711a4a898cebb88ac71d5a35783587d20
R0M-GH <rom.malla@outlook.com>
2023-04-03 13:25:59 -0700

    Revert "DOLLAZ ON MY HEAD - Gunna (Wunna)"
    This reverts commit 2687271b520245eb8ee06f1ed6f3cc1a51cb2dca.

--- scoutingapp/lib/screens/homepage.dart
+++ scoutingapp/lib/screens/homepage.dart
@@ -8,56 +8,88 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var padding = MediaQuery.of(context).padding;
-    double h1 =
-        MediaQuery.of(context).size.height - padding.top - padding.bottom;
-    double w1 =
-        MediaQuery.of(context).size.width - padding.left - padding.right;
-
     return CupertinoPageScaffold(
         child: Center(
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          const Center(
-            child: Text(
-              'FRCScout',
-              style: TextStyle(
-                  fontFamily: 'Jockey One', fontSize: 80, color: Colors.black),
-            ),
-          ),
-          Center(
-            child: Image.asset(
-              'assets/images/chargedup.png',
-              width: 300,
-            ),
-          ),
-          Center(
-              child: CupertinoButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const MenuPage()),
-              );
-            },
-            color: Colors.black,
-            child: const Text(
-              'ENERGIZE',
-              style: TextStyle(
-                  fontFamily: 'Jockey One',
-                  fontSize: 40,
-                  color: Color(0xff93D500)),
-            ),
-          )),
-          const Center(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const SizedBox(height: 50.0),
+            const Center(
               child: Text(
-            'Dev: R0M-GH\nVersion: Beta V 2023.0.1.3',
-            style: TextStyle(
-                fontFamily: 'Jockey One', fontSize: 20, color: Colors.black),
-            textAlign: TextAlign.center,
-          )),
-        ],
-      ),
+                'FRCScout',
+                style: TextStyle(
+                    fontFamily: 'Jockey One',
+                    fontSize: 80,
+                    color: Colors.black),
+              ),
+            ),
+            const SizedBox(height: 32.0),
+            Center(
+              child: Image.asset(
+                'assets/images/chargedup.png',
+                width: 300,
+              ),
+            ),
+            const SizedBox(height: 32.0),
+            Center(
+                child: CupertinoButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const MenuPage()),
+                );
+              },
+              color: Colors.black,
+              child: const Text(
+                'ENERGIZE',
+                style: TextStyle(
+                    fontFamily: 'Jockey One',
+                    fontSize: 40,
+                    color: Color(0xff93D500)),
+              ),
+            )),
+            const SizedBox(height: 80.0),
+            const Align(
+                alignment: Alignment.bottomCenter,
+                child: Text(
+                  'Dev: R0M-GH\nVersion: Beta V 2023.0.0.1',
+                  style: TextStyle(
+                      fontFamily: 'Jockey One',
+                      fontSize: 20,
+                      color: Colors.black),
+                  textAlign: TextAlign.center,
+                )),
+            Center(
+              child: Image.asset(
+                'assets/images/chargedup.png',
+                width: 300,
+              ),
+            ),
+            Center(
+                child: CupertinoButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const MenuPage()),
+                );
+              },
+              color: Colors.black,
+              child: const Text(
+                'ENERGIZE',
+                style: TextStyle(
+                    fontFamily: 'Jockey One',
+                    fontSize: 40,
+                    color: Color(0xff93D500)),
+              ),
+            )),
+            const Center(
+                child: Text(
+              'Dev: R0M-GH\nVersion: Beta V 2023.0.1.3',
+              style: TextStyle(
+                  fontFamily: 'Jockey One', fontSize: 20, color: Colors.black),
+              textAlign: TextAlign.center,
+            )),
+          ]),
     ));
   }
 }
--- scoutingapp/lib/screens/menupage.dart
+++ scoutingapp/lib/screens/menupage.dart
@@ -2,90 +2,17 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:scoutingapp/screens/pitscout.dart';
 
-import 'matchscout.dart';
+import 'homepage.dart';
 
 class MenuPage extends StatelessWidget {
   const MenuPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    var padding = MediaQuery.of(context).padding;
-    double h1 =
-        MediaQuery.of(context).size.height - padding.top - padding.bottom;
-    double w1 =
-        MediaQuery.of(context).size.width - padding.left - padding.right;
-
-    return CupertinoPageScaffold(
-        navigationBar: const CupertinoNavigationBar(
-          middle: Text('Main Menu'),
-        ),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              // SizedBox(height: (h1 / 48)),
-              // const Center(
-              //   child: Text(
-              //     'Main Menu',
-              //     style: TextStyle(
-              //         fontFamily: 'Jockey One',
-              //         fontSize: 80,
-              //         color: Colors.black),
-              //   ),
-              // ),
-              Center(
-                  child: CupertinoButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const MatchScout()),
-                  );
-                },
-                color: Colors.black,
-                child: const Text(
-                  'Match Scout',
-                  style: TextStyle(
-                      fontFamily: 'Jockey One',
-                      fontSize: 40,
-                      color: Color(0xff93D500)),
-                ),
-              )),
-              Center(
-                  child: CupertinoButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const PitScout()),
-                  );
-                },
-                color: Colors.black,
-                child: const Text(
-                  'Pit Scout',
-                  style: TextStyle(
-                      fontFamily: 'Jockey One',
-                      fontSize: 40,
-                      color: Color(0xff93D500)),
-                ),
-              )),
-              Center(
-                  child: CupertinoButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const MenuPage()),
-                  );
-                },
-                color: Colors.black,
-                child: const Text(
-                  'Admin',
-                  style: TextStyle(
-                      fontFamily: 'Jockey One',
-                      fontSize: 40,
-                      color: Color.fromARGB(255, 255, 68, 68)),
-                ),
-              )),
-            ],
-          ),
-        ));
+    return const CupertinoPageScaffold(
+      backgroundColor: CupertinoColors.systemTeal,
+      navigationBar: CupertinoNavigationBar(middle: Text('Main Menu')),
+      child: Center(),
+    );
   }
 }
